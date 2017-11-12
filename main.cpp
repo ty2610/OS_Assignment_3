40a7183dc883fcdc5f0e182dd24dd61fa4cff6b2
Tyler Green <gree0209@stthomas.edu>
2017-11-12 17:14:34 -0600

    Organized create process function. changed to vectors because that is going to be much better to implement and work with

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <vector>
 #include <string>
 
 //This is a CPP that will be compiled under c++ standard 11
 //compilable with g++ -o main main.cpp -std=c++11
 
 using namespace std;
-
-bool isNumber(const string& s);
-void takeCommand(int argc, char *argv[]);
 
 typedef struct Process {
     int PID;
@@ -20,11 +18,9 @@ typedef struct Process {
     int waitTime;
     int cpuTime;
     int remainTime;
-    //MUST ADD A DATATYPE to hold the amount of bursts
-    //and an array of ints that counts how long each cpu/io burst takes.
     int ioBursts;
-    int *ioBurstTimes;
-    int *cpuburstTimes;
+    vector<int> ioBurstTimes;
+    vector<int> cpuburstTimes;
     int startTime;
     int cpuBursts;
 } Process;
@@ -42,6 +38,10 @@ struct CommandInput {
     int timeSlice;
 }commandInput;
 
+bool isNumber(const string& s);
+void takeCommand(int argc, char *argv[]);
+vector<Process> createProcesses();
+
 int main(int argc, char *argv[]) {
     commandInput.cores = 0;
     commandInput.processors = 0;
@@ -53,36 +53,8 @@ int main(int argc, char *argv[]) {
     //checks to see if there are arguments, and error checks
     //Look over, I think this works, but it may be flawed
 
-    int firstProcesses = floor(commandInput.processors/3);
-    Process *processCollection = new Process[commandInput.processors];
-    for(int i=0; i<commandInput.processors; i++){
-        Process tempProcess;
-        tempProcess.PID = 1024 +i;
-        if(i<=firstProcesses){
-            tempProcess.state = "Ready";
-            tempProcess.startTime = 0;
-        } else {
-            tempProcess.state = "Not Created";
-            tempProcess.startTime = (rand() % 7501) + 500;
-        }
-        if(commandInput.algorithm==3){
-            tempProcess.priority = rand() % 5;
-        } else {
-            tempProcess.priority = 0;
-        }
-        tempProcess.ioBursts = (rand() % 9) + 2;
-        tempProcess.cpuBursts = tempProcess.ioBursts + 1;
-        int *cpuBursts = new int[tempProcess.cpuBursts];
-        int *ioBursts = new int[tempProcess.ioBursts];
-        for(int j=0; j<tempProcess.cpuBursts; j++) {
-            cpuBursts[j] = (rand() % 5001) + 1000;
-        }
-        for(int j =0; j<tempProcess.ioBursts; j++) {
-            ioBursts[j] = (rand() % 5001) + 1000;
-        }
-        //WILL ADD THIS PROCESS TO VECTOR. WILL EVENTUALLY BE ADDED TO QUEUE
-        //WILL BE MOVED TO OWN FUNCTION
-    }
+    vector<Process> processCollection = createProcesses();
+
 
     //THE OUTPUT SHOULD BE PUT IN IT'S OWN METHOD
     Process process;
@@ -115,7 +87,6 @@ bool isNumber(const string& s)
 
 void takeCommand(int argc, char *argv[]) {
     string argumentIndicators[] = {"-c","-p","-s","-o","-t"};
-    int amountOfArgumentIndicators = 5;
     bool atArgument = false;
     if(argc > 1) {
         for(int i=1; i<argc; i++) {
@@ -130,6 +101,9 @@ void takeCommand(int argc, char *argv[]) {
                 if(string(argv[i-1]) == "-c"){
                     if(string(argv[i])=="1" || string(argv[i])=="2" || string(argv[i])=="3" || string(argv[i])=="4") {
                         commandInput.cores = stoi(argv[i]);
+                    } else if (commandInput.cores != 0) {
+                        cout << "You can not specify an argument twice." << endl;
+                        exit(3);
                     } else {
                         cout << "The given argument for -c must be an integer and 1-4. " << argv[i] << endl;
                         exit(3);
@@ -139,6 +113,9 @@ void takeCommand(int argc, char *argv[]) {
                         int processorHolder = stoi(string(argv[i]));
                         if(processorHolder<1 || processorHolder>24){
                             cout << "The given number for argument -p must be greater than 0 but smaller than 25: " << argv[i] << endl;
+                            exit(3);
+                        } else if (commandInput.processors != 0) {
+                            cout << "You can not specify an argument twice." << endl;
                             exit(3);
                         } else {
                             commandInput.processors = processorHolder;
@@ -153,6 +130,9 @@ void takeCommand(int argc, char *argv[]) {
                         if(algorithmHolder<0 || algorithmHolder>3){
                             cout << "The given number for argument -s must be smaller than 4: " << argv[i] << endl;
                             exit(3);
+                        } else if (commandInput.algorithm != -1) {
+                            cout << "You can not specify an argument twice." << endl;
+                            exit(3);
                         } else {
                             commandInput.algorithm = algorithmHolder;
                         }
@@ -166,6 +146,9 @@ void takeCommand(int argc, char *argv[]) {
                         if(contextSwitchHolder<100 || contextSwitchHolder>1000){
                             cout << "The given number for argument -o must be smaller than 1001 and greater than 99: " << argv[i] << endl;
                             exit(3);
+                        } else if (commandInput.contextSwitch != 0) {
+                            cout << "You can not specify an argument twice." << endl;
+                            exit(3);
                         } else {
                             commandInput.contextSwitch = contextSwitchHolder;
                         }
@@ -174,9 +157,6 @@ void takeCommand(int argc, char *argv[]) {
                         exit(3);
                     }
                 } else if(string(argv[i-1]) == "-t"){
-                    //may need if check to see if the given algorithm is round robin
-                    //if it is, then don't do any error checking on the given value
-                    //because we wont be using it anyways
                     if(isNumber(string(argv[i]))) {
                         int timeSliceHolder = stoi(string(argv[i]));
                         if(timeSliceHolder<200 || timeSliceHolder>2000){
@@ -185,6 +165,9 @@ void takeCommand(int argc, char *argv[]) {
                         } else {
                             commandInput.timeSlice = timeSliceHolder;
                         }
+                    } else if (commandInput.timeSlice != 0) {
+                        cout << "You can not specify an argument twice." << endl;
+                        exit(3);
                     } else {
                         cout << "The given command for argument -t is not an integer " << argv[i] << endl;
                         exit(3);
@@ -200,8 +183,39 @@ void takeCommand(int argc, char *argv[]) {
         cout << "You must have arguments" << endl;
         exit(3);
     }
-    if(commandInput.cores==0 || commandInput.algorithm==0 || commandInput.processors==0 || commandInput.contextSwitch==0 || (commandInput.timeSlice==0 && commandInput.algorithm!=-1)) {
+    if(commandInput.cores==0 || commandInput.algorithm==0 || commandInput.processors==0 || commandInput.contextSwitch==0 || (commandInput.timeSlice==0 && commandInput.algorithm==0)) {
         cout << "The correct amount of arguments was not provided" << endl;
         exit(3);
     }
+}
+
+vector<Process> createProcesses() {
+    int firstProcesses = floor(commandInput.processors/3);
+    vector<Process> processCollection;
+    for(int i=0; i<commandInput.processors; i++){
+        Process tempProcess;
+        tempProcess.PID = 1024 +i;
+        if(i<=firstProcesses){
+            tempProcess.state = "Ready";
+            tempProcess.startTime = 0;
+        } else {
+            tempProcess.state = "Not Created";
+            tempProcess.startTime = (rand() % 7501) + 500;
+        }
+        if(commandInput.algorithm==3){
+            tempProcess.priority = rand() % 5;
+        } else {
+            tempProcess.priority = 0;
+        }
+        tempProcess.ioBursts = (rand() % 9) + 2;
+        tempProcess.cpuBursts = tempProcess.ioBursts + 1;
+        for(int j=0; j<tempProcess.cpuBursts; j++) {
+            tempProcess.cpuburstTimes.insert(tempProcess.cpuburstTimes.end(),(rand() % 5001) + 1000);
+        }
+        for(int j =0; j<tempProcess.ioBursts; j++) {
+            tempProcess.ioBurstTimes.insert(tempProcess.ioBurstTimes.end(),(rand() % 5001) + 1000);
+        }
+        processCollection.insert(processCollection.end(),tempProcess);
+    }
+    return processCollection;
 }
